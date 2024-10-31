bf8f63fc699fb5bb8f1b8e5e6632d2eddd4eafb7
igor medzinov <medzinov@gmail.com>
2024-10-31 17:36:31 +0300

    Cognitive Complexity fix atempt 1

--- games/brain-prime.js
+++ games/brain-prime.js
@@ -6,33 +6,41 @@ import {
   } from '../src/index.js';
 
   
-function isPrime(num) {
+const isPrime =(num)=> {
     for (let i = 2; i < num; i++) {
       if (num % i === 0) return false;
     }
     return num !== 1;
   }
   
-const playPrime = () => {
-  console.log("Answer 'yes' if the number is prime, otherwise answer 'no.")
-    let correctAnswers = 0
-    for (let i = 0; i < maxAttempts; i += 1){
-    const number =  generateRandomNumber(1, 20)
-    askQuestion(`${number}`)
-    const userAnswer = getUserAnswer()
-    const correctAnswer = isPrime(number) ? 'yes' : 'no'
-
-    if(!checkAnswer(userAnswer, correctAnswer)){
-        return
-      }else{
-        correctAnswers +=1
+  const playPrime = () => {
+    console.log("Answer 'yes' if the number is prime, otherwise answer 'no.");
+  
+    let correctAnswers = 0;
+  
+    for (let i = 0; i < maxAttempts; i++) {
+      const number = generateRandomNumber(1, 20);
+      askQuestion(`${number}`);
+      
+      if (processAnswer(number)) {
+        correctAnswers += 1;
+      } else {
+        return;
       }
+  
       if (correctAnswers === maxAttempts) {
         console.log(`Congratulations! ${userName}`);
+        return;
       }
     }
-}
-
+  };
+  
+  const processAnswer = (number) => {
+    const userAnswer = getUserAnswer();
+    const correctAnswer = isPrime(number) ? 'yes' : 'no';
+    
+    return checkAnswer(userAnswer, correctAnswer);
+  };
 
 playPrime()
 export default playPrime
